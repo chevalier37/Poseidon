33ff9eff8e1960e00b59a05f56aff4f6415cb177
Jean-Benoit <jean.benoit.roussat@gmail.com>
2020-05-12 08:37:49 +0200

    Mise en place des tests

---
+++ Poseidon2/src/main/java/com/poseidon/controllers/UserController.java
@@ -0,0 +1,53 @@
+package com.poseidon.controllers;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+
+import com.poseidon.domain.User;
+import com.poseidon.services.UserService;
+
+@RestController
+public class UserController {
+	
+	@Autowired
+	private UserService userService;
+
+	@GetMapping("/user/list")
+    public List<User> home(){
+        return userService.findAllUser();
+    }
+
+    @PostMapping("/user/add")
+    public User addUser(@RequestBody User user) {
+        return userService.addUser(user);
+    }
+
+    @GetMapping("/user/findId/{id}")
+    public Optional<User> showUpdateForm(@PathVariable("id") Integer id) {
+        return userService.getUserById(id);
+    }
+
+    @PutMapping("/user/update/{id}")
+    public User updateUser(@PathVariable("id") Integer id, @RequestBody User user) {
+    	user.setId(id);
+        return userService.addUser(user);
+    }
+
+    @DeleteMapping("/user/delete/{id}")
+    public void  deleteBid(@PathVariable("id") Integer id) {
+    	Optional<User> user = userService.getUserById(id);
+    	int userId = user.get().getId();
+    	userService.deleteUser(userId);
+    }
+
+}
